5132b5292ca12defa14fda6c5c9e8fd28d81bbe2
dionysiospapadopoulos <dionempapadop@gmail.com>
2022-04-19 21:53:56 +0100

    all field within div have been set for the distinct page. There is no style yet. Changes need to me made and collaborators icons added

--- client/src/containers/DistinctTaskPage.js
+++ client/src/containers/DistinctTaskPage.js
@@ -15,12 +15,40 @@ const DistinctTaskPage = () => {
 
         <div>
             <h3>{task.title}</h3>
-            
-        
             <input type="checkbox" checked={task.completed}/>
-             
-            
-            
+             <div className="distinctTaskFieldBox">
+                <p>{ task.type }</p>
+
+                {task.date?
+                <p>{ task.date }</p>
+                :
+                null
+                }
+
+                {task.time?
+                <p>{ task.time }</p>
+                :
+                null
+                }
+             </div>
+
+             <div>
+                 <h4>Collaborators</h4>
+             </div>
+
+             <div>
+                 <h4>Category</h4>
+                 <p>{ task.category }</p>
+             </div>
+
+             <div>
+                 <h4>Priority</h4>
+                 <p>{ task.priority }</p>
+             </div>
+
+             <div>
+                 <p>{ task.description }</p>
+             </div>
         </div>
     )
 }
