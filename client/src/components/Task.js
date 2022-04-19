61cfab4f166a509ac3be27c6c42eacc0055b9cbb
dionysiospapadopoulos <dionempapadop@gmail.com>
2022-04-19 22:02:00 +0100

    I dont know how we are supposed to add a goal. I do not know where I should be getting the priorities lists and categories from. I stop for today

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,5 +1,4 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Task = ({ task }) => {
     const navigate = useNavigate();
@@ -14,15 +13,8 @@ const Task = ({ task }) => {
     return (
             <div>
                 <input type="checkbox" id="horns" name="horns"/>
-               {/* <Link to={{pathname: `/task/:${task.title}`,
-                state: {task:task}}}><label for="horns"> <button onClick={onTaskClick}>{ task.title }</button> </label></Link>  */}
-                {/* <Link to={{pathname: `/task/:${task.title}`}}
-                state= {{task:task}}> */}
                 <label for="horns"> <button onClick={onTaskClick}>{ task.title }</button> </label>
-                {/* </Link>  */}
             </div>
-            // <Link  to={{pathname: `/question/:${question.id}`,
-            //     state: {question:question.id}}}><button className="main_button_style" onClick={passingQid}> {questionToUse.answered? "View Question": "Answer Question"}</button></Link>
       )
 }
 
