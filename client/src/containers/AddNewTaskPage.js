c55223acac964d7fc3cfff2963f991705aa8a9d8
dionysiospapadopoulos <dionempapadop@gmail.com>
2022-04-20 14:04:13 +0100

    date and time are saving properly

--- client/src/containers/AddNewTaskPage.js
+++ client/src/containers/AddNewTaskPage.js
@@ -5,7 +5,7 @@ import DropDownMenu from "../components/DropDownMenu";
 const AddNewTaskPage = () => {
     // const [taskToBeEdited, setTaskToBeEdited] = useState(null);
     const [taskTitle, setTaskTitle] = useState("");
-    const [taskCategory, setTaskCategory] = useState("General");
+    const [taskCategory, setTaskCategory] = useState(null);
     const [taskPriority, setTaskPriority] = useState(null);
 
     const [hasDescription, setHasDescription] = useState(false);
@@ -22,14 +22,24 @@ const AddNewTaskPage = () => {
     const location = useLocation();
     const categories = location.state.categories;
     const priorities = location.state.priorities;
-    
 
-    const setValueFromDropDown = (choosenOption, field) => {
-        if(field==="category"){
+    // let year = Date.prototype.getFullYear()
+    // let month = Date.prototype.getMonth()
+    // let day = Date.prototype.getDate()
+    
+    useEffect(() => {
+        setHasDescription(false)
+        
+    }, []);
+
+    const setCategoryFromDropDown = (choosenOption) => {
             setTaskCategory(choosenOption);
-        }if(field==="priority"){
+       
+        
+    }
+
+    const setPriorityFromDropDown = (choosenOption) => {
             setTaskPriority(choosenOption);
-        }
         
     }
     // const location = useLocation();
@@ -42,6 +52,7 @@ const AddNewTaskPage = () => {
     //     }
         
     // }, [location]);
+   
 
 
     return (
@@ -52,11 +63,41 @@ const AddNewTaskPage = () => {
                 <input type="text" name="taskTitle" id="taskTitle" value={taskTitle} onChange={e=> setTaskTitle(e.target.value)} required/>
                 {/* <h3>{taskTitle}</h3> */}
 
-                <label htmlFor="taskTitle">Task Category</label>
-                <DropDownMenu options={ categories } setValueFromDropDown={(choosenOption, categories)=> setValueFromDropDown(choosenOption, "category")} />
+                <label htmlFor="taskCategory">Task Category</label>
+                <DropDownMenu options={ categories } setValueFromDropDown={(choosenOption, categories)=> setCategoryFromDropDown(choosenOption)} />
 
-                <label htmlFor="taskTitle">Task Priority</label>
-                <DropDownMenu options={ priorities } setValueFromDropDown={(choosenOption, categories)=> setValueFromDropDown(choosenOption, "priority")} />
+                <label htmlFor="taskPriority">Task Priority</label>
+                <DropDownMenu options={ priorities } setValueFromDropDown={(choosenOption, categories)=> setPriorityFromDropDown(choosenOption)} />
+
+                {hasDescription?
+                <>
+                    <label htmlFor="taskDescription">Task Description</label>
+                    <input type="text" name="taskDescription" id="taskDescription" value={taskDescription} onChange={e=> setTaskDescription(e.target.value)}/>
+                    <button onClick={()=> setHasDescription(false)}>Remove Description</button>
+                </>
+                :
+                <button onClick={()=> setHasDescription(true)}>Add Description</button>
+                }
+
+                {hasDate?
+                <>
+                    <label htmlFor="taskDate">Task Date</label>
+                    <input type="date" name="taskDate" id="taskDate" value={taskDate} onChange={e=> setTaskDate(e.target.value)}/>
+                    <button onClick={()=> setHasDate(false)}>Remove Date</button>
+                </>
+                :
+                <button onClick={()=> setHasDate(true)}>Add Date</button>
+                }
+
+                {hasTime?
+                <>
+                    <label htmlFor="taskDate">Task Time</label>
+                    <input type="time" name="taskDate" id="taskDate" value={taskTime} onChange={e=> setTaskTime(e.target.value)}/>
+                    <button onClick={()=> setHasTime(false)}>Remove Date</button>
+                </>
+                :
+                <button onClick={()=> setHasTime(true)}>Add Time</button>
+                }       
 
                 <button type="submit">Create Task </button>
             </form>
