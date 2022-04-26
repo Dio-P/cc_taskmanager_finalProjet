469cf34a21bfb2ab2048eb2cc665dd5060dc93ac
Rachel McDonald <rach-mcdonald@outlook.com>
2022-04-26 16:18:06 +0100

    Positioning of menu items and colours updated

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
+import { FaPlus } from "react-icons/fa";
 
 const Menu = ({ closeMenuFunction, categories, priorities, goals, goalTypesList }) => {
     const navigate = useNavigate();
@@ -23,30 +24,31 @@ const Menu = ({ closeMenuFunction, categories, priorities, goals, goalTypesList 
     return(
         <div>
             <ul className="MENU showMenuNav space-x-8 lg:flex">
-            <button onClick={()=> closeMenuFunction()}>X</button>
+            <button onClick={()=> closeMenuFunction()}><FaPlus className='menu-close'/></button>
+            <p className='menu-title'>Menu</p>
             <li>
-                <button className="addNewTaskButton" onClick={()=> onClick("/profile")}> Profile </button>
+                <button className="addNewTaskButton text-lg -m-4" onClick={()=> onClick("/profile")}> Profile </button>
             </li>
             <li>
                 <a href="/">View Tasks</a>
             </li>
             <li>
-                <button className="addNewTaskButton" onClick={()=> onClick("/task/createNewTask")}> Add New Tasks </button>
+                <button className="addNewTaskButton text-lg -m-4" onClick={()=> onClick("/task/createNewTask")}> Add New Tasks </button>
             </li>
             <li>
-                <button className="allCategoriesButton" onClick={()=> onClick("/categories")}> Categories </button>
+                <button className="allCategoriesButton text-lg -m-4" onClick={()=> onClick("/categories")}> Categories </button>
             </li>
             <li>
-                <button className="addNewCategoryButton" onClick={()=> onClick("/category/createNewCategory")}> Add New Category </button>
+                <button className="addNewCategoryButton text-lg -m-4" onClick={()=> onClick("/category/createNewCategory")}> Add New Category </button>
             </li>
             <li>
-                <button className="allGoalsButton" onClick={()=> onClick("/goals")}> Goals </button>
+                <button className="allGoalsButton text-lg -m-4" onClick={()=> onClick("/goals")}> Goals </button>
             </li>
             <li>
-                <button className="allGoalsButton" onClick={()=> onClick("/goal/createNewGoal")}> Add New Goal </button>
+                <button className="allGoalsButton text-lg -m-4" onClick={()=> onClick("/goal/createNewGoal")}> Add New Goal </button>
             </li>
             <li>
-                <button onClick={() => logout({ returnTo: window.location.origin })}>
+                <button className='log-out' onClick={() => logout({ returnTo: window.location.origin })}>
                     Log Out
                 </button>
             </li>
